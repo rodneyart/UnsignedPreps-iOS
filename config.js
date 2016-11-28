69c247f3d72c0fc6bb750cbc91286739a45ac0b8
rodneyart <rodneyartdesign@gmail.com>
2016-11-28 11:40:20 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'unsigned-preps',
+		wp_ws_url : 'http://app.unsignedpreps.org/wp-appkit-api/unsigned-preps',
+		wp_url : 'http://app.unsignedpreps.org',
+		theme : 'q-ios',
+		version : '1.0.0',
+		app_title : 'Unsigned Preps',
+		app_platform : 'ios',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'ms8{dW#|Dh2pKTiTH<NBU)(M;&fWOT}giGHb~9*YE>x^pCA4$O1E,p+w1M5Vl2g]',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
